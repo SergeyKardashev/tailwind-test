dae448aa3e90106629ac470919004dcdfc1b86b4
SergeyKardashev <angeldeep@yandex.ru>
2024-07-10 18:55:10 +0700

    feat: add theme-sensitive blocks and fix links to output css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media', // или 'class' для ручного управления темой
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
